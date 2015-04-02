de28818b5e06907312db5facc537d6916b865df8
Maciej Kurek <maciejk77@gmail.com>
2015-04-02 18:55:52 +0100

    Final

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,16 @@
+class RomanNumerals
+
+  ROMAN_NUMERALS = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
+  
+  def self.convert (number)
+    roman_numeral = ""
+    ROMAN_NUMERALS.each do |key, value|
+      (number / key).times { roman_numeral << value; number -= key}
+    end
+      roman_numeral
+  end
+
+
+end
+
+
